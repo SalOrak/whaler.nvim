9d4dfb6fcd656a84dc0de1d5fe0aaa30280ca102
GCBallesteros <dev+github@maxwellrules.com>
2023-12-20 07:35:44 +0100

    style: Apply stylua

--- lua/telescope/_extensions/whaler.lua
+++ lua/telescope/_extensions/whaler.lua
@@ -1,15 +1,14 @@
 local has_telescope, telescope = pcall(require, "telescope")
 
 if not has_telescope then
-    error('This plugin requires nvim-telescope/telescope.nvim')
+    error "This plugin requires nvim-telescope/telescope.nvim"
 end
 
-local whaler = require('telescope._extensions.whaler.main')
+local whaler = require "telescope._extensions.whaler.main"
 
 return telescope.register_extension {
     setup = whaler.setup,
     exports = {
-        whaler =  whaler.whaler,
-    }
+        whaler = whaler.whaler,
+    },
 }
-
--- lua/telescope/_extensions/whaler/utils.lua
+++ lua/telescope/_extensions/whaler/utils.lua
@@ -1,20 +1,19 @@
 local _fn = vim.fn
 
-
 -- Whaler
 local M = {}
 
--- Whaler utility functions 
+-- Whaler utility functions
 
-M.merge_tables_by_index= function(t1, t2)
+M.merge_tables_by_index = function(t1, t2)
     local out = {}
     local idx = 1
-    for _, v in ipairs(t1)do
+    for _, v in ipairs(t1) do
         out[idx] = v
         idx = idx + 1
     end
 
-    for _, v in ipairs(t2)do
+    for _, v in ipairs(t2) do
         out[idx] = v
         idx = idx + 1
     end
@@ -24,11 +23,11 @@ end
 M.merge_tables_by_key = function(t1, t2)
     local out = {}
 
-    for k, v in pairs(t2)do
+    for k, v in pairs(t2) do
         out[k] = v
     end
 
-    for k, v in pairs(t1)do
+    for k, v in pairs(t1) do
         out[k] = v
     end
 
@@ -39,9 +38,9 @@ M.parse_directory = function(dir)
     dir = dir or ""
     local tmp = ""
     for i = 1, _fn.strlen(dir) do
-        local c1 = dir:sub(i,i)
-        local c2 = dir:sub(i+1,i+1)
-        if not ((c1  == c2 or i == _fn.strlen(dir)) and c1 == '/') then
+        local c1 = dir:sub(i, i)
+        local c2 = dir:sub(i + 1, i + 1)
+        if not ((c1 == c2 or i == _fn.strlen(dir)) and c1 == "/") then
             tmp = tmp .. c1
         end
     end
