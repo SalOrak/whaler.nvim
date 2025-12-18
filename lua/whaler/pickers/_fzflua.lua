28a950e8399ad9cee2dee58bf0ca9ed130a8c420
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-12-18 16:14:41 +0100

    feat: added fzf_lua picker implementation

---
+++ lua/whaler/pickers/_fzflua.lua
@@ -0,0 +1,79 @@
+local Whaler = require'whaler'
+local State = require'whaler.state'
+local Fzf = require'fzf-lua'
+
+-- TODO: Sane config defaults for fzf-lua 
+
+local defaults = {
+    prompt = "Whaler >> ",
+    previewer = false,
+    header = "",
+    winopts = {
+        border = "none",
+        preview = {
+            horizontal = "right:40%"
+        }
+    },
+    actions = {
+        ["default"] = {
+            function(selected)
+                local dirs_map = State:get().dirs_map
+                local run_opts = State:get().run_opts
+
+                local display = selected[1] 
+                local path = dirs_map[selected[1]]
+                local run_opts = run_opts or {}
+
+                -- Whaler allows executing commands after the selection.
+                -- Because fzf_lua does not delete the window *before* caliing
+                -- this function we should force the deletion of the buffer
+                -- manually
+                vim.api.nvim_buf_delete(0, {force = true})
+
+                Whaler.select(path, display, run_opts)
+            end
+        }
+    },
+    fn_format_entry = function(entry)
+        if entry.alias then
+            return (
+                "["
+                .. entry.alias
+                .. "] "
+                .. vim.fn.fnamemodify(entry.path, ":t")
+            )
+        end
+
+        return entry.path
+    end
+}
+
+
+local picker = function(dirs, run_opts)
+
+    local run_opts = vim.tbl_deep_extend('force', defaults, run_opts or {})
+    local dirs_map = {}
+
+    for k,v in pairs(dirs) do
+        local key = defaults.fn_format_entry(v)
+
+        -- Path should never be null
+        assert(dirs[k].path ~= nil, "Directory path is never null")
+        local value = dirs[k].path 
+
+        dirs_map[key] = value
+    end
+
+    --- Update the global state
+    State:set({
+        dirs_map = dirs_map,
+        run_opts = run_opts,
+    })
+
+    Fzf.fzf_exec(vim.tbl_keys(dirs_map), run_opts)
+end
+
+
+return {
+        picker = picker
+    }
