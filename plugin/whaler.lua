1f437e0f8b38c07272cd001b561af6388061c32b
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-12-18 16:55:09 +0100

    fix: WhalerSwitch user command switches to current buffer path

--- plugin/whaler.lua
+++ plugin/whaler.lua
@@ -3,10 +3,11 @@ vim.api.nvim_create_user_command("Whaler", function(ctx)
   end, {})
 
 vim.api.nvim_create_user_command("WhalerSwitch", function(ctx)
-    P(ctx.args)
-    -- require("whaler").switch(ctx.fargs)
+    local bufnr = vim.api.nvim_get_current_buf()
+    local filename =  vim.api.nvim_buf_get_name(bufnr)
+    local clean = string.gsub(filename,".*://", "")
+    local path = vim.fs.abspath(clean)
+    local parent = vim.fs.dirname(path)
+    require("whaler").switch(parent)
   end,
-  {
-      complete = "dir",
-      nargs = "*",
-})
+  { desc = "Whaler: Changes project to current buffer path"})
